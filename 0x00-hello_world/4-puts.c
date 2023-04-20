e980525d7390fe12dd227afe1925ce22076b7518
HAJAR-t <hajartahiri79@gmail.com>
2023-04-20 14:26:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A C program that prints a line, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	Return(0);
+} 
+
